ce3398e0e9fbc6f44ad834221cff19fa972d8d74
Luc CHENG <luc.cheng7@etu.univ-lorraine.fr>
2019-10-14 21:12:47 +0200

    Suppression de la variable res

--- src/p2048/model/CubeGrille.java
+++ src/p2048/model/CubeGrille.java
@@ -191,10 +191,9 @@ public class CubeGrille implements Runnable, Serializable {
                 i++;
             }
             deplacerRecursif(rangeeSuiv, direction, compteur);
-        } else if (compteur<getTaille()-1 && !res) {
+        } else if (compteur<getTaille()-1) {
             deplacerRecursif(getCasesExtremites(direction), direction, compteur+1);
         }
-        return res;
     }
     
     public void deplacer(int direction) {
