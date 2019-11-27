a1166dacf343a411f1516f27fbcd5fdc8a2495c4
Nicolas QUEIGNEC <nicolas.queignec3@etu.univ-lorraine.fr>
2019-11-27 16:37:20 +0100

    Contrôleur coop début

---
+++ src/p2048/controleur/ReseauRejoindreControleur.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2048.controleur;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Nicolas QUEIGNEC
+ */
+public class ReseauRejoindreControleur implements Controleur {
+    @FXML
+    private Label id1;
+    @FXML
+    private Label id2;
+    @FXML
+    private Label id3;
+    @FXML
+    private Label j1;
+    @FXML
+    private Label j2;
+    @FXML
+    private Label j3;
+    @FXML
+    private Label nbJoueurs1;
+    @FXML
+    private Label nbJoueurs2;
+    @FXML
+    private Label nbJoueurs3;
+    @FXML
+    private Label page;
+    @FXML
+    private Label nbPages;
+    @FXML
+    private Button prec;
+    @FXML
+    private Button suiv;
+    
+}
