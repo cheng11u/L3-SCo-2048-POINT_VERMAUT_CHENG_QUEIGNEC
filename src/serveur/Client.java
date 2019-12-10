39ea872758781b52643db2d78ab3569c4b018434
cleve_000 <cleve_000@10.11.120.53>
2019-12-10 14:59:58 +0100

    correction orthographe

--- src/serveur/Client.java
+++ src/serveur/Client.java
@@ -17,7 +17,7 @@ import p2048.model.reseau.JoueurPoints;
  * @author Nicolas QUEIGNEC
  */
 public class Client implements Runnable {
-    private PrintWriter envoyeut;
+    private PrintWriter envoyeur;
     private BufferedReader receveur;
     private Socket socket;
     private Partie partieEnCours;
@@ -25,7 +25,7 @@ public class Client implements Runnable {
     
     public Client(Socket sock) {
         try {
-            this.envoyeut=new PrintWriter(sock.getOutputStream());
+            this.envoyeur=new PrintWriter(sock.getOutputStream());
             this.receveur=new BufferedReader(new InputStreamReader(sock.getInputStream()));
             this.socket=sock;
         } catch (IOException ex) {
@@ -71,8 +71,8 @@ public class Client implements Runnable {
     }
     
     public void envoyerMessage(String message) {
-        this.envoyeut.println(message);
-        this.envoyeut.flush();
+        this.envoyeur.println(message);
+        this.envoyeur.flush();
     }
     
     public void deconnecter(){
@@ -80,7 +80,7 @@ public class Client implements Runnable {
         try {
             this.socket.close();
             this.receveur.close();
-            this.envoyeut.close();
+            this.envoyeur.close();
             Main.clients.remove(this);
         } catch (IOException ex) {
             System.err.println("Erreur");
